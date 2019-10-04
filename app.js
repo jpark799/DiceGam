741b261b3b5a07444ed4bb3ac95cb54017371920
jpark799 <jason.j.park79@gmail.com>
2019-10-04 16:31:54 +0900

    logging 2 random numbers from 1-6

---
+++ app.js
@@ -0,0 +1,5 @@
+let randomNumber1 = Math.floor(Math.random() * Math.floor(6) + 1)
+let randomNumber2 = Math.floor(Math.random() * Math.floor(6) + 1)
+
+console.log(`Random Number 1 is ${randomNumber1}`)
+console.log(`Random Number 2 is ${randomNumber2}`)
